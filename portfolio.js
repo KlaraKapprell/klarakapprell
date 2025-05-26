3563ff75d5f21a72b81e67e338a56db58b2adfdc
KlaraKapprell <167532859+KlaraKapprell@users.noreply.github.com>
2025-05-26 12:46:25 +0200

    Version 5.0

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // --- DOWNLOAD CV PDF ---
+  cvTrigger.addEventListener("click", function () {
+    const cvUrl = "media/KlaraKapprell_Portfolio.pdf";
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = "Klara-Kapprell_Portfolio.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+
+}); 
